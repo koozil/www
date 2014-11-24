a2eccea22bcfe61068483134b9cadfd71d8335c4
Filip Kozłowski <kooziloo@gmail.com>
2014-11-24 11:16:39 +0100

    zmiany w układzie strony

--- index.php
+++ index.php
@@ -1,58 +1,52 @@
 ﻿<?php
   include 'nav.php';
   ?>
-		<div class="container">
+		<div class="container- fluid">
   <h1>Specjalizacje- PHP</h1>
-  
+
   <div class="alert alert-info" role="alert">Poniżej podane zadania są zrobione w niekoniecznie dobrej kolejności.
-  Więcej informacji w pliku  <a href="https://github.com/koozil/PHP"> README</a> </div> 
-  <div class="alert alert-info" role="alert">Zawsze możesz zmienić wersję kolorystyczną strony (work in progress)  <a href="indexalt.php">"Więc chodź, pomaluj mi stronkę :) "</a> </div>
+  Więcej informacji w pliku  <a href="https://github.com/koozil/PHP"> README</a> </div>
+<div class="alert alert-info" role="alert">Zawsze możesz zmienić wersję kolorystyczną strony (work in progress)  <a href="indexalt.php">"Więc chodź, pomaluj mi stronkę :) "</a> </div>
 
-<div class="btn-group btn-group-justified">
-  <div class="btn-group">
-    <button  type="button" class="btn btn-default"><h3><a href="petla.php">Petla</a></h3></button>
-  </div>
-  <div class="btn-group">
-   <button type="button" class="btn btn-default"><h3><a href="test.php">Teścik</a></h3></button>
-  </div>
-  <div class="btn-group">
-  <button type="button" class="btn btn-default"><h3><a href="cwicz2.php">Ćwiczenia 1 & 2</a></h2></h3></button>
-  </div>
-  <div class="btn-group">
-  <button type="button" class="btn btn-default"><h3><a href="cwicz4.php">Ćwiczenie 4</a></h2></h3></button>
-  </div>
-  <div class="btn-group">
-  <button type="button" class="btn btn-default"><h3><a href="cwicz5.php">Ćwiczenie 5</a></h2></h3></button>
-  </div>
-  <div class="btn-group">
-  <button type="button" class="btn btn-default"><h3><a href="cwiczget2.php">Ćwiczenie GET 2</a></h2></h3></button>
-  </div>
-  
-<div class="btn-group">
-  <button type="button" class="btn btn-default"><h3><a href="cwicz6.php">Ćwiczenie 6</a></h2></h3></button>
-  </div>  
-  <div class="btn-group">
-  <button type="button" class="btn btn-default"><h3><a href="post.php">POST</a></h2></h3></button>
-  </div>
-  <div class="btn-group">
-  <button type="button" class="btn btn-default"><h3><a href="pole.php">Ile wynosi pole...</a></h2></h3></button>
-  </div>
-  
-  
-  <div class="btn-group">   <button type="button" class="btn btn-default"><h3><a href="#">nic #2</a></h3></button></div>
-</div>
-</div>
-</div>
-<div class="container">
+
 
 </div>
 
-</left>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Simple Sidebar</h1>
+                        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
+                        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                    </div>
+                </div>
+        
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+
 </body>
 <footer class="bs-docs-footer" role="contentinfo">
   <div class="container">
   <ul class="bs-docs-footer-links muted">
-   
+
       <li><a href="https://github.com/koozil">GitHub- koozil</a></li>
 	  <li><a href="https://github.com/twbs/bootstrap/releases">Releases- Bootstrap</a></li>
   </ul>
--- nav.php
+++ nav.php
@@ -1,1 +1,56 @@
-﻿<!Doctype hmtl><html><head><title>KOZA</title><link href="../css/bootstrap.min.css" rel="stylesheet"><link href="../assets/css/docs.min.css" rel="stylesheet"><!-- Custom styles for this template -->    <link href="justified-nav.css" rel="stylesheet"><script src="../assets/js/ie10-viewport-bug-workaround.js"></script><script src="../assets/js/ie-emulation-modes-warning.js"></script><link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png"><link rel="icon" href="/favicon.ico"></head><body>    <div class="container">      <div class="masthead">        <h1 class="text-muted">KOZA</h1>        <div role="navigation">          <ul class="nav nav-justified">            <li><a href="index.php">Home</a></li>       <li><a href="https://github.com/koozil">My GitHub</a></li>            <li><a href="about.php">About</a></li><li><a href="https://www.facebook.com/Strychrock?ref=hl">The Strychers </a></li></ul></div></div>
+﻿<!Doctype hmtl><html><head><title>KOZA</title>
+   <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/docs.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+     <link href="justified-nav.css" rel="stylesheet">
+     <script src="../assets/js/ie10-viewport-bug-workaround.js"></script>
+     <script src="../assets/js/ie-emulation-modes-warning.js"></script>
+     <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png">
+     <link rel="icon" href="/favicon.ico"></head><body>
+      <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Start Bootstrap
+                    </a>
+                </li>
+                <li>
+                    <a href="petla.php">Pętla</a>
+                </li>
+                <li>
+                    <a href="test.php">Php info</a>
+                </li>
+                <li>
+                    <a href="cwicz2.php">Ćwiczenie 1</a>
+                </li>
+                <li>
+                    <a href="cwicz5.php">Ćwiczenie 2</a>
+                </li>
+                <li>
+                    <a href="cwiczget2.php">Ćwiczenie 3</a>
+                </li>
+                <li>
+                    <a href="pole.php">Ćwiczenie 4</a>
+                </li>
+                <li>
+                    <a href="post.php">Ćwiczenie 5</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+          <div id="page-content-wrapper">
+            <div class="container-fluid">
+         <div class="masthead">
+                   <h1 class="text-muted">KOZA</h1>
+                         <div role="navigation">
+                     <ul class="nav nav-justified">
+                               <li><a href="index.php">Home</a></li>
+                                <li><a href="https://github.com/koozil">My GitHub</a></li>
+                                    <li><a href="about.php">About</a></li><li>
+                                    <a href="https://www.facebook.com/Strychrock?ref=hl">The Strychers </a>
+                                      </li></ul></div></div>
